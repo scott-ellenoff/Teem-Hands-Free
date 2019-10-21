1697ea4443f89febd377d13c92e9a66f49524c43
Scott Ellenoff <scott@epihub.com>
2019-10-21 18:49:32 -0500

    Commit

---
+++ window2.js
@@ -0,0 +1,41 @@
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+$(document).ready(function(){
+    $(".start").click(function(){
+        for(i=0; i<6; i++){
+            sleep(10000);
+            var rand = Math.ceil(Math.random()*5+1)
+            if(rand == 1.0){
+                window.navigator.vibrate([200,200]);
+                console.log("wow"+i);
+            }
+            else if(rand == 2.0){
+                console.log("like"+i);
+                window.navigator.vibrate([200,200,200]);
+            }
+            else if(rand == 3.0){
+                console.log("love"+i);
+                window.navigator.vibrate([200,200,200,200]);
+            }
+            else if(rand == 4.0){
+                console.log("angry"+i);
+                window.navigator.vibrate([200,200,200,200,200]);
+            }
+            else if(rand == 5.0){
+                console.log("sad"+i);
+                window.navigator.vibrate([200,200,200,200,200,200]);
+            }
+            else if(rand == 6.0){
+                console.log("laugh"+i);
+                window.navigator.vibrate([200,200,200,200,200,200,200]);
+            }
+        }
+        $(".start").hide();
+    });
+});
---
+++ window3.js
@@ -0,0 +1,58 @@
+var vibrate = false;
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+}
+$(document).ready(function(){
+    var choice = "";
+    $(".choose").unbind("click").on("click",function(){
+        if($(this).val() == "angry"){
+            choice = "angry";
+            window.navigator.vibrate([200,200]);
+            $(".wow").toggle();
+            $(".angry").hide();
+        }
+        else if($(this).val() == "wow"){
+            choice = "wow";
+            window.navigator.vibrate([200,200,200]);
+            $(".sad").toggle();
+            $(".wow").hide();
+        }
+        else if($(this).val() == "sad"){
+            choice = "sad";
+            window.navigator.vibrate([200,200,200,200]);
+            $(".like").toggle();
+            $(".sad").hide();
+        }
+        if($(this).val() == "like"){
+            choice = "like";
+            window.navigator.vibrate([200,200,200,200,200]);
+            $(".love").toggle();
+            $(".like").hide();
+        }
+        if($(this).val() == "love"){
+            choice = "love";
+            window.navigator.vibrate([200,200,200,200,200,200]);
+            $(".laugh").toggle();
+            $(".love").hide();
+        }
+        if($(this).val() == "laugh"){
+            choice = "laugh";
+            window.navigator.vibrate([200,200,200,200,200,200,200]);
+            $(".cancel").toggle();
+            $(".laugh").hide();
+        }
+        if($(this).val() == "cancel"){
+            choice = "";
+            $(".angry").toggle();
+            $(".cancel").hide();
+        }
+    });
+    $(".send").click(function(){
+        console.log(choice);
+    });
+});
