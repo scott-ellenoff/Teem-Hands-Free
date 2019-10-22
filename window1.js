2c852d0ac08604ebacba389c164833e25edff59d
Scott Ellenoff <scott@epihub.com>
2019-10-21 20:22:29 -0500

    Update window1.js

--- window1.js
+++ window1.js
@@ -1,30 +1,31 @@
 $(document).ready(function(){
+    var ios = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
     $(".button.send").click(function(){
         console.log("send");
         window.navigator.vibrate([200]);
     });
     $(".wow").click(function(){
         console.log("wow");
-        window.navigator.vibrate([200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200]);
     });
     $(".like").click(function(){
         console.log("like");
-        window.navigator.vibrate([200, 200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200, 200]);
     });
     $(".love").click(function(){
         console.log("love");
-        window.navigator.vibrate([200, 200, 200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200, 200, 200]);
     });
     $(".angry").click(function(){
         console.log("angry");
-        window.navigator.vibrate([200, 200, 200, 200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200, 200, 200, 200]);
     });
     $(".sad").click(function(){
         console.log("sad");
-        window.navigator.vibrate([200, 200, 200, 200, 200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200, 200, 200, 200, 200]);
     });
     $(".laugh").click(function(){
         console.log("laugh");
-        window.navigator.vibrate([200, 200, 200, 200, 200, 200, 200]);
+        if(!ios) window.navigator.vibrate([200, 200, 200, 200, 200, 200, 200]);
     });
 });
