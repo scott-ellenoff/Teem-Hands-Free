6560b0597db0cb747ae324126f6a44cad74b1805
Scott Ellenoff <scott@epihub.com>
2019-10-22 16:54:57 -0500

    Update window2.js

--- window2.js
+++ window2.js
@@ -1,24 +1,22 @@
-function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }
-  }
+
 $(document).ready(function(){
     $(".start").click(function(){
         $(".start").hide();
         window.navigator.vibrate([200,200,200]);  //like
-        sleep(10000);
-        window.navigator.vibrate([200,200,200,200,200,200]); //sad
-        sleep(10000);
-        window.navigator.vibrate([200,200]); //wow
-        sleep(10000);
-        window.navigator.vibrate([200,200,200,200]); //love
-        sleep(10000);
-        window.navigator.vibrate([200,200,200,200,200,200,200]); //laugh
-        sleep(10000);
-        window.navigator.vibrate([200,200,200,200,200]); //angry
+        setTimeout(function(){
+            window.navigator.vibrate([200,200,200,200,200,200]); //sad
+            setTimeout(() => {
+                window.navigator.vibrate([200,200]); //wow
+                setTimeout(() => {
+                    window.navigator.vibrate([200,200,200,200]); //love
+                    setTimeout(() => {
+                        window.navigator.vibrate([200,200,200,200,200,200,200]); //laugh
+                        setTimeout(() => {
+                            window.navigator.vibrate([200,200,200,200,200]); //angry
+                        }, 10000);
+                    }, 10000);
+                }, 10000);
+            }, 10000);
+        },10000);
     });
 });
